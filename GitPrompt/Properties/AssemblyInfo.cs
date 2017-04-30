b95ce79a09dec7b9675001f5e8df86c7c965195f
Geeven <geevensingh@hotmail.com>
2017-04-30 12:45:08 -0700

    Rename Prompt->GitPrompt

--- GitPrompt/Properties/AssemblyInfo.cs
+++ GitPrompt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Prompt")]
+[assembly: AssemblyTitle("GitPrompt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prompt")]
+[assembly: AssemblyProduct("GitPrompt")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
