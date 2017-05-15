e6e060d2349a18cb44886c60a92390ba1dea2945
Geeven <geevensingh@hotmail.com>
2017-05-14 22:37:48 -0700

    GitSwitchBranch: Add ability to follow an existing remote

--- GitSwitchBranch/Program.cs
+++ GitSwitchBranch/Program.cs
@@ -17,7 +17,7 @@ namespace GitSwitchBranch
 #endif
 
             string currentBranch = GitOperations.GetCurrentBranchName();
-            List<string> branches = new List<string>(GitOperations.GetLocalBranches("--sort committerdate"));
+            List<string> branches = new List<string>(GitOperations.GetLocalBranches());
             branches.Remove(currentBranch);
 
             Logger.LogLine(@"Currently on " + currentBranch);
@@ -27,6 +27,7 @@ namespace GitSwitchBranch
                 Logger.LogLine("\t" + (ii + 1) + " : " + branches[ii]);
             }
             Logger.LogLine("\tn : Make a new branch");
+            Logger.LogLine("\tf : Follow an existing remote branch");
             string input = Console.ReadKey().KeyChar.ToString().Trim().ToLower();
             Logger.LogLine(string.Empty);
             if (string.IsNullOrEmpty(input) || (input.ToLower() == "q"))
@@ -37,6 +38,12 @@ namespace GitSwitchBranch
             if (input.ToLower() == "n")
             {
                 MakeNewBranch();
+                return;
+            }
+
+            if (input.ToLower() == "f")
+            {
+                FollowExistingRemoteBranch();
                 return;
             }
 
@@ -62,6 +69,76 @@ namespace GitSwitchBranch
 
             GitOperations.SwitchBranch(branches[index - 1]);
             return;
+        }
+
+        private static void FollowExistingRemoteBranch()
+        {
+            string searchTerm = Environment.GetEnvironmentVariable("USERNAME").ToLower();
+#if DEBUG
+            searchTerm = "personal";
+#endif
+            string[] localBranches = StringHelper.ToLower(GitOperations.GetLocalBranches());
+            List<string> remoteBranches = new List<string>(GitOperations.GetRemoteBranches("--sort committerdate"));
+            for (int ii = 0; ii < remoteBranches.Count; ii++)
+            {
+                string lowerRemoteBranch = StringHelper.TrimStart(remoteBranches[ii].ToLower(), "origin/");
+                if (localBranches.Contains(lowerRemoteBranch))
+                {
+                    remoteBranches.RemoveAt(ii--);
+                }
+            }
+
+            List<string> matchingBranches = new List<string>();
+            foreach(string remoteBranch in remoteBranches)
+            {
+                if (remoteBranch.ToLower().Contains(searchTerm))
+                {
+                    matchingBranches.Add(remoteBranch);
+                }
+            }
+            if (matchingBranches.Count > 0)
+            {
+                Logger.LogLine(@"Select from the following:");
+                for (int ii = 0; ii < matchingBranches.Count; ii++)
+                {
+                    Logger.LogLine("\t" + (ii + 1) + " : " + matchingBranches[ii]);
+                }
+                Logger.Log("Or");
+            }
+            Logger.LogLine("Enter another branch name");
+            string prompt = Console.ReadLine().Trim();
+            if (string.IsNullOrEmpty(prompt) || (prompt.ToLower() == "q"))
+            {
+                return;
+            }
+
+            string basedOn = string.Empty;
+            if (remoteBranches.Contains(prompt))
+            {
+                basedOn = prompt;
+            }
+
+            if (string.IsNullOrEmpty(basedOn) && remoteBranches.Contains("origin/" + prompt))
+            {
+                basedOn = "origin/" + prompt;
+            }
+
+            if (string.IsNullOrEmpty(basedOn))
+            {
+                int index = -1;
+                if (int.TryParse(prompt, out index) && (index > 0) && (index <= matchingBranches.Count))
+                {
+                    basedOn = matchingBranches[index - 1];
+                }
+            }
+
+            if (string.IsNullOrEmpty(basedOn))
+            {
+                Logger.LogLine("Unable to find the given branch: " + prompt, Logger.LevelValue.Error);
+                return;
+            }
+
+            GitOperations.CreateBranch(StringHelper.TrimStart(basedOn, "origin/"), basedOn);
         }
 
         private static void MakeNewBranch()
