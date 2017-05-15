48fea675a9c2de0df25f1845b38f2ac4092145ae
Geeven <geevensingh@hotmail.com>
2017-05-14 22:49:15 -0700

    Tests: Add StringHelperTest

---
+++ Utilities-Tests/StringHelperTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Utilities;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utilities_Tests
+{
+    [TestClass]
+    public class StringHelperTest
+    {
+        private string[] MakeStringArray(uint count, string prefix)
+        {
+            List<string> result = new List<string>();
+            for (uint ii = 0; ii < count; ii++)
+            {
+                result.Add(prefix + "-" + ii.ToString());
+            }
+            return result.ToArray();
+        }
+
+        private void CompareArrays<T>(T[] first, T[] second)
+        {
+            Assert.AreEqual(first.Length, second.Length);
+            for (int ii = 0; ii < first.Length; ii++)
+            {
+                Assert.AreEqual(first[ii], second[ii]);
+            }
+        }
+
+        [TestMethod]
+        public void TestToLower()
+        {
+            CompareArrays(MakeStringArray(10, "lower-case"), StringHelper.ToLower(MakeStringArray(10, "lower-case")));
+            CompareArrays(MakeStringArray(10, "lower-case"), StringHelper.ToLower(MakeStringArray(10, "LOWER-CASE")));
+        }
+
+        [TestMethod]
+        public void TestTrimStart()
+        {
+            Assert.AreEqual("foo", StringHelper.TrimStart("foo", "bar"));
+            Assert.AreEqual("bar", StringHelper.TrimStart("foobar", "foo"));
+            Assert.AreEqual("foobar", StringHelper.TrimStart("foobar", "Foo"));
+            Assert.AreEqual("bar", StringHelper.TrimStart("foobar", "Foo", StringComparison.CurrentCultureIgnoreCase));
+            Assert.AreEqual("BAR", StringHelper.TrimStart("FOOBAR", "Foo", StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
