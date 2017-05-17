67d47b357ae4fd05bf35a6e9c823108d343a859b
Geeven Singh <geevensingh@hotmail.com>
2017-05-16 20:54:09 -0700

    GitStaleBranches: Improve logging output

--- GitStaleBranches/Program.cs
+++ GitStaleBranches/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Utilities;
+using System.Diagnostics;
 
 namespace GitStaleBranches
 {
@@ -30,14 +31,25 @@ namespace GitStaleBranches
                 branches = GitOperations.GetRemoteBranches();
             }
 
+            List<string> lines = new List<string>();
             foreach(string branch in branches)
             {
-                ProcessHelper proc = new ProcessHelper("git.exe", "log --since=" + aMonthAgoString + " " + branch);
-                if (proc.Go().Length == 0)
+                if (!branch.Contains(" -> "))
                 {
-                    Logger.LogLine("No changes in " + branch, Logger.LevelValue.Warning);
+                    Logger.Log(".", Logger.LevelValue.Verbose);
+                    ProcessHelper proc = new ProcessHelper("git.exe", "log -n 1 --since=" + aMonthAgoString + " " + branch);
+                    if (proc.Go().Length == 0)
+                    {
+                        string[] changeDescription = (new ProcessHelper("git.exe", "log --date=short --pretty=format:\" %an (%ae) was the last person to touch " + StringHelper.TrimStart(branch, "origin/") + " on %ad\" -n 1 " + branch)).Go();
+                        Debug.Assert(changeDescription.Length == 1);
+                        lines.Add(changeDescription[0]);
+
+                    }
                 }
             }
+            Logger.LogLine("", Logger.LevelValue.Verbose);
+            lines.Sort();
+            Logger.LogLine(lines.ToArray());
         }
     }
 }
