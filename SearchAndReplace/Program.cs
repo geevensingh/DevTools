f4f3135db643218528bf8d366760c1a9cdc94b95
Geeven Singh <geevensingh@hotmail.com>
2017-07-26 12:06:20 -0700

    Add SearchAndReplace app

---
+++ SearchAndReplace/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace SearchAndReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allFiles = new List<string>(GetAllFiles(new string[] { "h", "cpp", "idl" }));
+
+            foreach (string file in allFiles)
+            {
+                string text = File.ReadAllText(file);
+                int oldTextLength = text.Length - 1;
+                while (oldTextLength != text.Length)
+                {
+                    oldTextLength = text.Length;
+                    text = text.Replace(" \r\n", "\r\n");
+                }
+
+                File.WriteAllText(file, text, Utilities.IOHelper.GetEncoding(file));
+            }
+        }
+
+        static string[] GetAllFiles(string[] extensions)
+        {
+            List<string> allFiles = new List<string>();
+            foreach (string ext in extensions)
+            {
+                allFiles.AddRange(Directory.EnumerateFiles(@"S:\Repos\media.app_2\src", @"*." + ext, SearchOption.AllDirectories));
+            }
+            return allFiles.ToArray();
+        }
+
+    }
+}
