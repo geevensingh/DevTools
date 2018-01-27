323270d22c597e052b0aecb77b442ca8c6a2cdb8
Geeven Singh <geevensingh@hotmail.com>
2018-01-26 16:55:55 -0800

    Hack update

--- Hack/Program.cs
+++ Hack/Program.cs
@@ -11,58 +11,105 @@ namespace Hack
 {
     class Program
     {
+        struct Counts
+        {
+            public int files;
+            public int inserts;
+            public int deletes;
+            public string commit;
+
+            public Counts(string commitId, int fileCount, int insertCount, int deleteCount)
+            {
+                commit = commitId;
+                files = fileCount;
+                inserts = insertCount;
+                deletes = deleteCount;
+            }
+        }
+
         static int Main(string[] args)
         {
-#if false
-            string originalFilePath = @"S:\Repos\Sumner\zune\client\xaml\temp.txt";
-            List<string> lines = new List<string>(File.ReadAllLines(originalFilePath));
-            for (int ii = 0; ii < lines.Count; ii++)
+            Dictionary<string, List<Counts>> dict = new Dictionary<string, List<Counts>>();
+            ProcessHelper proc = new ProcessHelper("git.exe", "log -n 1200 --pretty=\"%H %ae %ad\"");
+            int maxAuthorLength = 0;
+            foreach (string line in proc.Go())
             {
-                string line = lines[ii];
-                if (line == "\"")
+                string[] splits = line.Split(new char[] { ' ' });
+
+                string commitId = splits[0];
+                string author = StringHelper.TrimEnd(splits[1], "@microsoft.com");
+                maxAuthorLength = Math.Max(maxAuthorLength, author.Length);
+
+                int fileCount, insertCount, deleteCount;
+                GitOperations.GetCommitStats(commitId, out fileCount, out insertCount, out deleteCount);
+                if (!dict.ContainsKey(author))
                 {
-                    lines.RemoveAt(ii--);
-                    continue;
+                    dict[author] = new List<Counts>();
                 }
-                line = line.TrimStart(new char[] { '\"' });
-                line = line.Replace("\"\"", "\"");
-                lines[ii] = line;
+                dict[author].Add(new Counts(commitId, fileCount, insertCount, deleteCount));
+                Logger.Log(".");
             }
-            lines.InsertRange(0, File.ReadAllLines(@"s:\prefix.txt"));
-            lines.AddRange(File.ReadAllLines(@"s:\suffix.txt"));
-            string newFile = originalFilePath.Replace(@".txt", @".xml");
-            File.WriteAllLines(newFile, lines.ToArray(), Encoding.UTF8);
-            File.Copy(newFile, originalFilePath.Replace(@"temp.txt", @"strings\en-US\resw\migrationStrings\resources.resw"), true /*overwrite*/);
-#endif
-            foreach (string filePath in GetAllFiles(@"S:\Repos\media.app\src\Generated\Idl\EntPlat.Idl", new string[] { "h"}))
+            Logger.LogLine(" done");
+
+            foreach (string author in dict.Keys)
             {
-                ProcessHelper proc = new ProcessHelper("git.exe", @"diff --unified=0 " + filePath);
-                string[] lines = proc.Go();
-                if (proc.ExitCode != 0 || proc.StandardError.Length != 0)
+                List<Counts> list = dict[author];
+                int totalFileCount = 0;
+                int totalInsertCount = 0;
+                int totalDeleteCount = 0;
+
+                foreach(Counts count in list)
                 {
-                    Logger.LogLine("Unable to diff " + filePath);
-                    return -1;
+                    totalFileCount += count.files;
+                    totalInsertCount += count.inserts;
+                    totalDeleteCount += count.deletes;
                 }
 
-                bool foundChange = false;
-                for (int ii = 0; ii < lines.Length; ii++)
+                string fileCountString = PluralStringCheck(totalFileCount, "file changed", "files changed");
+                string insertCountString = PluralStringCheck(totalFileCount, "insertion(+)", "insertions(+)");
+                string deleteCountString = PluralStringCheck(totalFileCount, "deletion(+)", "deletions(+)");
+                string netCountString = PluralStringCheck((totalInsertCount - totalDeleteCount), "net line changed", "net lines changed");
+                Logger.LogLine(("Author: " + author).PadRight(maxAuthorLength + 10) + fileCountString.PadRight(20) + insertCountString.PadRight(20) + deleteCountString.PadRight(20) + netCountString);
+            }
+
+            foreach (string author in dict.Keys)
+            {
+                List<Counts> list = dict[author];
+                int totalFileCount = 0;
+                int totalInsertCount = 0;
+                int totalDeleteCount = 0;
+
+                foreach (Counts count in list)
                 {
-                    if (lines[ii].StartsWith("@@") && ii + 2 < lines.Length)
-                    {
-                        if (lines[ii+1].StartsWith(@"-/* Compiler settings for") && lines[ii + 2].StartsWith(@"+/* Compiler settings for"))
-                        {
-                            continue;
-                        }
-                        foundChange = true;
-                    }
+                    totalFileCount += count.files;
+                    totalInsertCount += count.inserts;
+                    totalDeleteCount += count.deletes;
                 }
-                if (!foundChange)
+
+                Logger.LogLine(author.PadRight(maxAuthorLength + 10) + totalFileCount.ToString().PadRight(20) + totalInsertCount.ToString().PadRight(20) + totalDeleteCount.ToString().PadRight(20));
+            }
+
+            foreach (string author in dict.Keys)
+            {
+                Logger.LogLine("------------------------------------");
+                Logger.LogLine("Author: " + author);
+                List<Counts> list = dict[author];
+                foreach(Counts count in list)
                 {
-                    RevertFile(filePath);
-                    RevertFile(StringHelper.TrimEnd(filePath, @".h") + @".winmd");
+                    Logger.LogLine(count.commit.PadRight(45) + count.files.ToString().PadRight(20) + count.inserts.ToString().PadRight(20) + count.deletes.ToString().PadRight(20));
                 }
             }
+
             return 0;
+        }
+
+        static string PluralStringCheck(int count, string singularString, string pluralString)
+        {
+            if (count > 1)
+            {
+                return count + " " + pluralString;
+            }
+            return count + " " + singularString;
         }
 
         static bool RevertFile(string filePath)
