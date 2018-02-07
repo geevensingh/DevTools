5083ccd1c1f9584a66e11611e48d7fff6ef32f15
Geeven Singh <geevensingh@hotmail.com>
2018-02-07 13:00:56 -0800

    Utilities: Add TaskExtensions

---
+++ Utilities/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Utilities
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            task.ContinueWith(t => { Debug.WriteLine(t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
