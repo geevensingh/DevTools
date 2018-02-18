76e11d69723ce72e60a3a61225634ecd3aad0d05
Geeven <geevensingh@hotmail.com>
2018-02-18 12:22:17 -0800

    JsonViewer: Add find match next/previous

--- JsonViewer/View/MainWindowToolbar.xaml.cs
+++ JsonViewer/View/MainWindowToolbar.xaml.cs
@@ -14,7 +14,9 @@
 
         private string _findMatchText = string.Empty;
 
-        private int _currentHitIndex = 0;
+        private int? _currentHitIndex = null;
+
+        private int? _currentIndex = null;
 
         public MainWindowToolbar()
         {
@@ -32,6 +34,8 @@
         public Visibility ShowFindControls { get => string.IsNullOrEmpty(_findMatchText) ? Visibility.Collapsed : Visibility.Visible; }
 
         public string FindMatchText { get => _findMatchText; }
+
+        public int? CurrentIndex { get => _currentIndex; }
 
         private void OnSettingsPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
@@ -51,10 +55,32 @@
             _mainWindow = (MainWindow)this.DataContext;
             Debug.Assert(_mainWindow != null);
 
+            _mainWindow.Tree.SelectedItemChanged += OnTreeSelectedItemChanged;
+
             _mainWindow.Finder.PropertyChanged += OnFinderPropertyChanged;
             FindTextBox.Text = _mainWindow.Finder.Text;
 
             this.HighlightParentsButton.IsChecked = Properties.Settings.Default.HighlightSelectedParents;
+        }
+
+        private void OnTreeSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Debug.Assert(sender == _mainWindow.Tree);
+            TreeViewData newSelectedData = (TreeViewData)e.NewValue;
+
+            _currentHitIndex = null;
+            int? newIndex = null;
+            if (newSelectedData != null)
+            {
+                newIndex = newSelectedData.JsonObject.OverallIndex;
+                if (newSelectedData.JsonObject.IsFindMatch)
+                {
+                    _currentHitIndex = _mainWindow.Finder.Hits.IndexOf(newSelectedData.JsonObject);
+                }
+            }
+
+            NotifyPropertyChanged.SetValue(ref _currentIndex, newIndex, "CurrentIndex", this, this.PropertyChanged);
+            UpdateFindMatches();
         }
 
         private void OnFinderPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -67,7 +93,7 @@
                     break;
                 case "HitCount":
                 case "Hits":
-                    _currentHitIndex = 0;
+                    _currentHitIndex = null;
                     UpdateFindMatches();
                     break;
             }
@@ -78,11 +104,17 @@
             Finder finder = _mainWindow.Finder;
             if (finder.HitCount == 0)
             {
+                Debug.Assert(!_currentHitIndex.HasValue);
                 NotifyPropertyChanged.SetValue(ref _findMatchText, string.Empty, new string[] { "ShowFindControls", "FindMatchText" }, this, this.PropertyChanged);
                 return;
             }
 
-            string findMatchText = $"{_currentHitIndex + 1} / {finder.HitCount}";
+            string findMatchText = $"?? / {finder.HitCount}";
+            if (_currentHitIndex.HasValue)
+            {
+                findMatchText = $"{_currentHitIndex.Value + 1} / {finder.HitCount}";
+            }
+
             NotifyPropertyChanged.SetValue(ref _findMatchText, findMatchText, new string[] { "ShowFindControls", "FindMatchText" }, this, this.PropertyChanged);
         }
 
@@ -93,18 +125,63 @@
 
         private void FindNextButton_Click(object sender, RoutedEventArgs e)
         {
-            _currentHitIndex = (_currentHitIndex + 1) % _mainWindow.Finder.Hits.Count;
-            JsonObject hit = _mainWindow.Finder.Hits[_currentHitIndex];
+            GetHitIndexRange(out int previous, out int next);
+
+            _currentHitIndex = (previous + 1) % _mainWindow.Finder.Hits.Count;
+            JsonObject hit = _mainWindow.Finder.Hits[_currentHitIndex.Value];
             _mainWindow.Tree.ExpandToItem(hit.ViewObject);
             UpdateFindMatches();
         }
 
         private void FindPreviousButton_Click(object sender, RoutedEventArgs e)
         {
-            _currentHitIndex = (_currentHitIndex + _mainWindow.Finder.Hits.Count - 1) % _mainWindow.Finder.Hits.Count;
-            JsonObject hit = _mainWindow.Finder.Hits[_currentHitIndex];
+            GetHitIndexRange(out int previous, out int next);
+
+            _currentHitIndex = (next + _mainWindow.Finder.Hits.Count - 1) % _mainWindow.Finder.Hits.Count;
+            JsonObject hit = _mainWindow.Finder.Hits[_currentHitIndex.Value];
             _mainWindow.Tree.ExpandToItem(hit.ViewObject);
             UpdateFindMatches();
+        }
+
+        private void GetHitIndexRange(out int previous, out int next)
+        {
+            if (_currentHitIndex.HasValue)
+            {
+                previous = _currentHitIndex.Value;
+                next = _currentHitIndex.Value;
+                return;
+            }
+
+            Debug.Assert(_mainWindow.Finder.HitCount > 0);
+            if (_mainWindow.Finder.HitCount == 1)
+            {
+                previous = 1;
+                next = 1;
+                return;
+            }
+
+            previous = -1;
+            if (_currentIndex.HasValue)
+            {
+                Debug.Assert(_mainWindow.Finder.HitCount == _mainWindow.Finder.Hits.Count);
+                for (int ii = 0; ii < _mainWindow.Finder.Hits.Count; ii++)
+                {
+                    JsonObject foo = _mainWindow.Finder.Hits[ii];
+                    if (foo.OverallIndex <= _currentIndex.Value)
+                    {
+                        previous = ii;
+                    }
+                }
+            }
+
+            previous = (previous + _mainWindow.Finder.HitCount) % _mainWindow.Finder.HitCount;
+            next = (previous + 1 + _mainWindow.Finder.HitCount) % _mainWindow.Finder.HitCount;
+
+            Debug.Assert(previous >= 0);
+            Debug.Assert(previous < _mainWindow.Finder.HitCount);
+
+            Debug.Assert(next >= 0);
+            Debug.Assert(next < _mainWindow.Finder.HitCount);
         }
 
 #pragma warning disable SA1201 // Elements must appear in the correct order
