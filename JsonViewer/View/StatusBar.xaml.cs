b6662384101edcbe55f9d5ed1872f4d1a0480956
Geeven <geevensingh@hotmail.com>
2018-02-18 23:08:12 -0800

    JsonViewer: extract StatusBar to its own class

---
+++ JsonViewer/View/StatusBar.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace JsonViewer.View
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for StatusBar.xaml
+    /// </summary>
+    public partial class StatusBar : UserControl, INotifyPropertyChanged
+    {
+        private MainWindow _mainWindow = null;
+
+        public StatusBar()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string CurrentIndex
+        {
+            get
+            {
+                int? currentIndex = _mainWindow?.Tree?.SelectedIndex;
+                if (currentIndex.HasValue)
+                {
+                    return currentIndex.Value.ToString();
+                }
+
+                return "--";
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _mainWindow = (MainWindow)this.DataContext;
+            Debug.Assert(_mainWindow != null);
+
+            _mainWindow.Tree.PropertyChanged += OnTreePropertyChanged;
+        }
+
+        private void OnTreePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "SelectedIndex":
+                    NotifyPropertyChanged.FirePropertyChanged("CurrentIndex", this, this.PropertyChanged);
+                    break;
+            }
+        }
+    }
+}
