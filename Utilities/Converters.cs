3be8faa86d9a85f07780f5e8f2db8f9253794a71
Geeven Singh <geevensingh@hotmail.com>
2018-02-21 19:19:12 -0800

    Utilities: Add Converters.ToDouble

---
+++ Utilities/Converters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Converters
+    {
+        public static double? ToDouble(object obj)
+        {
+            if (obj.GetType() == typeof(int))
+            {
+                return (int)obj;
+            }
+
+            if (obj.GetType() == typeof(float))
+            {
+                return (float)obj;
+            }
+
+            if (obj.GetType() == typeof(double))
+            {
+                return (double)obj;
+            }
+
+            Debug.Fail("invalid double: " + obj.ToString());
+            return null;
+        }
+    }
+}
