08f592798ac867e4791654b5ac6ecee51dce5c2b
Geeven Singh <geevensingh@hotmail.com>
2018-03-09 15:11:45 -0800

    Update file versions

--- JsonViewer/Properties/AssemblyInfo.cs
+++ JsonViewer/Properties/AssemblyInfo.cs
@@ -54,8 +54,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 #pragma warning restore SA1005 // Single line comments must begin with single space
