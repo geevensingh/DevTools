35b98fd4fd1358a4196dfe6ff72a8a83766d7475
Geeven Singh <geevensingh@hotmail.com>
2022-04-21 09:11:54 -0700

    Update Wordle

---
+++ Wordle/Program - V1.cs
@@ -0,0 +1,109 @@
+﻿
+var wrongPlace = new HashSet<(char, int)>()
+{
+    //( 'c', 0 ),
+    //( 'a', 1 ),
+    //( 'r', 2 ),
+    //( 'e', 3 ),
+    //( 's', 4 ),
+
+    //( 'm', -1 ),
+    //( 'n', 2 ),
+    //( 'y', 4 ),
+};
+
+var rightPlace = new Dictionary<char, int>()
+{
+    //{ 's', 0 },
+    //{ 'h', 1 },
+    //{ 'a', 2 },
+    //{ 'e', 3 },
+    //{ 'e', 4 },
+};
+
+var badLetters = new HashSet<char>()
+{
+    //'c',
+    //'a',
+    //'r',
+    //'e',
+    //'s',
+
+    //'t',
+    //'l',
+
+    //'k',
+    //'v',
+    //'t',
+    
+    //'c',
+    //'h',
+    //'i',
+    //'f',
+    //'s',
+    //'t',
+    //'n',
+};
+
+string[] words = await File.ReadAllLinesAsync("Scrabble-Words.txt");
+words = words
+    .Select(x => x.ToLower().Trim())
+    .Where(x => x.Length == 5)
+    .Where(x => x == x.ToLower())
+    .ToArray();
+
+foreach (var pair in rightPlace)
+{
+    words = words.Where(x => x[pair.Value] == pair.Key).ToArray();
+}
+
+foreach (var ch in badLetters)
+{
+    words = words.Where(x => !x.Contains(ch)).ToArray();
+}
+
+foreach (var pair in wrongPlace)
+{
+    words = words.Where(x => x.Remove(pair.Item2, 1).Contains(pair.Item1)).ToArray();
+}
+
+Dictionary<int, Dictionary<char, int>> charCountLookup = new Dictionary<int, Dictionary<char, int>>();
+
+for (int ii = 0; ii < 5; ii++)
+{
+    charCountLookup.Add(ii, words.Select(x => x[ii]).Distinct().ToDictionary(x => x, x => 0));
+    foreach (char ch in charCountLookup[ii].Keys)
+    {
+        charCountLookup[ii][ch] = words.Count(x => x[ii] == ch);
+    }
+
+    int maxValueForChar = charCountLookup[ii].Max(x => x.Value);
+    Console.WriteLine($"pos: {ii}    most frequent letter: {string.Join(",", charCountLookup[ii].Where(x => x.Value == maxValueForChar).Select(x => x.Key))}");
+}
+
+var wordLookup = new Dictionary<string, int>();
+foreach (string word in words)
+{
+    int index = 0;
+    int wordValue = 0;
+    foreach (char ch in word)
+    {
+        wordValue += charCountLookup[index++][ch];
+    }
+    if (word.ToCharArray().Distinct().Count() == word.Length)
+    {
+        wordValue = (int)Math.Ceiling(wordValue * 1.0);
+    }
+    wordLookup.Add(word, wordValue);
+}
+
+foreach (var word in wordLookup.OrderBy(x => x.Value).Select(x => x.Key))
+{
+    Console.WriteLine($"{word} : {wordLookup[word]}");
+}
+
+Console.WriteLine();
+int maxWordScore = wordLookup.Max(x => x.Value);
+Console.WriteLine($"best word is '{string.Join(",", wordLookup.Where(x => x.Value == maxWordScore).Select(x => x.Key))}'");
+
+Console.WriteLine();
--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,40 +1,49 @@
-﻿
+﻿using System.Diagnostics;
+
+string[] words = await File.ReadAllLinesAsync("Scrabble-Words.txt");
+words = words
+    .Select(x => x.ToLower().Trim())
+    .Where(x => x.Length == 5)
+    .Where(x => x == x.ToLower())
+    .ToArray();
+
 var wrongPlace = new HashSet<(char, int)>()
 {
-    //( 'c', 0 ),
+    //( 't', 0 ),
     //( 'a', 1 ),
-    ( 'r', 2 ),
+    //( 'r', 2 ),
     //( 'e', 3 ),
     //( 's', 4 ),
 
-    //( 'm', 0 ),
+    //( 'm', -1 ),
     //( 'n', 2 ),
     //( 'y', 4 ),
 };
 
-var rightPlace = new Dictionary<char, int>()
+var rightPlace = new HashSet<(char, int)>()
 {
-    { 'f', 0 },
-    //{ 'o', 1 },
-    //{ 'u', 2 },
-    { 'e', 3 },
-    { 'r', 4 },
+    //{ 's', 0 },
+    //{ 'h', 1 },
+    //{ 'a', 2 },
+    //{ 'e', 3 },
+    //{ 'e', 4 },
 };
 
 var badLetters = new HashSet<char>()
 {
-    'c',
-    'a',
+    //'t',
+    //'a',
     //'r',
     //'e',
-    's',
+    //'s',
 
-    'i',
-    'v',
+    //'t',
+    //'l',
 
-    'l',
-    'y',
-    //'m',
+    //'k',
+    //'v',
+    //'t',
+
     //'c',
     //'h',
     //'i',
@@ -44,65 +53,94 @@ var badLetters = new HashSet<char>()
     //'n',
 };
 
-string[] words = await File.ReadAllLinesAsync("Scrabble-Words.txt");
-words = words
-    .Select(x => x.ToLower().Trim())
-    .Where(x => x.Length == 5)
-    .Where(x => x == x.ToLower())
-    .ToArray();
 
-foreach (var pair in rightPlace)
+string wordUsed = "cares";
+Console.WriteLine($"Word used: [{wordUsed}]");
+while (true)
 {
-    words = words.Where(x => x[pair.Value] == pair.Key).ToArray();
-}
-
-foreach (var ch in badLetters)
-{
-    words = words.Where(x => !x.Contains(ch)).ToArray();
-}
-
-foreach (var pair in wrongPlace)
-{
-    words = words.Where(x => x.Remove(pair.Item2, 1).Contains(pair.Item1)).ToArray();
-}
-
-Dictionary<int, Dictionary<char, int>> charCountLookup = new Dictionary<int, Dictionary<char, int>>();
-
-for (int ii = 0; ii < 5; ii++)
-{
-    charCountLookup.Add(ii, words.Select(x => x[ii]).Distinct().ToDictionary(x => x, x => 0));
-    foreach (char ch in charCountLookup[ii].Keys)
+    Console.WriteLine("Pattern returned: ('.' == miss, '-' == wrong place, 'x' == correct letter");
+    string pattern = Console.ReadLine();
+    Debug.Assert(pattern.Length == 5);
+    Debug.Assert(pattern.All(x => x == '.' || x == '-' || x == 'x'));
+    for (int ii = 0; ii < pattern.Length; ii++)
     {
-        charCountLookup[ii][ch] = words.Count(x => x[ii] == ch);
+        switch (pattern[ii])
+        {
+            case '.':
+                badLetters.Add(wordUsed[ii]);
+                break;
+            case '-':
+                wrongPlace.Add((wordUsed[ii], ii));
+                break;
+            case 'x':
+                rightPlace.Add((wordUsed[ii], ii));
+                break;
+            default:
+                Debug.Fail("What?");
+                break;
+        }
     }
 
-    int maxValueForChar = charCountLookup[ii].Max(x => x.Value);
-    Console.WriteLine($"pos: {ii}    most frequent letter: {string.Join(",", charCountLookup[ii].Where(x => x.Value == maxValueForChar).Select(x => x.Key))}");
-}
-
-var wordLookup = new Dictionary<string, int>();
-foreach (string word in words)
-{
-    int index = 0;
-    int wordValue = 0;
-    foreach (char ch in word)
+    foreach (var pair in rightPlace)
     {
-        wordValue += charCountLookup[index++][ch];
+        words = words.Where(x => x[pair.Item2] == pair.Item1).ToArray();
     }
-    if (word.ToCharArray().Distinct().Count() == word.Length)
+
+    foreach (var ch in badLetters)
     {
-        wordValue = (int)Math.Ceiling(wordValue * 1.4);
+        words = words.Where(x => !x.Contains(ch)).ToArray();
     }
-    wordLookup.Add(word, wordValue);
+
+    foreach (var pair in wrongPlace)
+    {
+        words = words.Where(x => x.Remove(pair.Item2, 1).Contains(pair.Item1)).ToArray();
+    }
+
+    Dictionary<string, double> scoreLookup = new Dictionary<string, double>();
+    foreach (string word in words)
+    {
+        scoreLookup[word] = words.Where(x => x != word).Sum(x => CompareWords(word, x));
+        if (word.Distinct().Count() == word.Length)
+        {
+            scoreLookup[word] = Math.Round(scoreLookup[word] * 1.2, 2);
+        }
+    }
+
+    scoreLookup = scoreLookup.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+    var bestWords = scoreLookup.TakeLast(50);
+    foreach (var word in bestWords)
+    {
+        Console.WriteLine($"{word.Key} : {word.Value}");
+    }
+
+    string bestWord = bestWords.Last().Key;
+    Console.WriteLine($"Word used: [{bestWord}]");
+    wordUsed = Console.ReadLine();
+    if (string.IsNullOrEmpty(wordUsed))
+    {
+        wordUsed = bestWord;
+    }
+    Debug.Assert(wordUsed.Length == 5);
 }
 
-foreach (var word in wordLookup.OrderBy(x => x.Value).Select(x => x.Key))
+
+
+
+static int CompareWords(string guess, string answer)
 {
-    Console.WriteLine($"{word} : {wordLookup[word]}");
+    Debug.Assert(guess.Length == answer.Length);
+    int score = 0;
+    for (int ii = 0; ii < guess.Length; ii++)
+    {
+        if (guess[ii] == answer[ii])
+        {
+            score += 3;
+        }
+        else if (answer.Contains(guess[ii]))
+        {
+            score++;
+        }
+    }
+
+    return score;
 }
-
-Console.WriteLine();
-int maxWordScore = wordLookup.Max(x => x.Value);
-Console.WriteLine($"best word is '{string.Join(",", wordLookup.Where(x => x.Value == maxWordScore).Select(x => x.Key))}'");
-
-Console.WriteLine();
